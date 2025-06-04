56bf01c99e3fafd7e0ea5bcd96ae64e49ac6e06a
MIR20100 <dildorad890@gamil.com>
2025-06-04 18:43:09 +0500

    ebreb

--- news/urls.py
+++ news/urls.py
@@ -2,21 +2,25 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls.i18n import i18n_patterns
 
-# Tillarni o‘zgartirish uchun URL (masalan: /i18n/setlang/)
+
+from django.conf.urls.i18n import i18n_patterns
+from debug_toolbar.toolbar import debug_toolbar_urls
+
 urlpatterns = [
     path('i18n/', include('django.conf.urls.i18n')),
 ]
 
-# Asosiy routinglar (tilga bog‘langan holda)
+
 urlpatterns += i18n_patterns(
     path('admin/', admin.site.urls),
-    path('', include('app.urls')),          # <--- BU TO‘G‘RI
-    path('', include('user.urls')),     # agar `ders_app` mavjud bo‘lsa
+    path("ckeditor5/", include('django_ckeditor_5.urls')),
+    path('', include('app.urls')),          
+    path('', include('user.urls')),     
 )
 
-# Rasm/media/static fayllar uchun
+urlpatterns += debug_toolbar_urls()
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
